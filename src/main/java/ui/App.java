3eebc7fae2eaf88178329f8ebc4de8bad2004378
Josh Heinrichs (jih320) <josh.heinrichs@usask.ca>
2015-05-11 15:00:50 -0600

    Moved toolbar into main window

--- src/main/java/ui/App.java
+++ src/main/java/ui/App.java
@@ -4,9 +4,12 @@ import geometry.Point;
 import geometry.Segment;
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
 import javafx.scene.Cursor;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToolBar;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -16,6 +19,7 @@ import javafx.stage.Stage;
 import modelAdapters.DelaunayTriangulationAdapter;
 import models.CompleteGraph;
 import models.DelaunayTriangulation;
+import tools.Tool;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -44,7 +48,27 @@ public class App extends Application {
         stage.setTitle(modelAdapter.getName());
         stage.show();
 
+        ToolBar toolBar = new ToolBar();
+        toolBar.setPrefWidth(600/9*16);
+        toolBar.setMaxWidth(Double.MAX_VALUE);
+
+        ArrayList<Tool> tools = modelAdapter.getTools();
+        for (int i = 0; i < tools.size(); i++) {
+            Button button = new Button(tools.get(i).getName());
+            final int tool = i;
+            button.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                public void handle(MouseEvent t) {
+                    modelAdapter.setSelectedTool(tool);
+                }
+            });
+            toolBar.getItems().add(button);
+        }
+
+        toolBar.setOrientation(Orientation.HORIZONTAL);
+
+
         root.getChildren().add(modelAdapter.draw());
+        root.getChildren().add(toolBar);
     }
 
     public static void main(String[] args) {
