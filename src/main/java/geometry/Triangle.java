60116faae33abb8a90a6e4cf282ffa725fdf8656
Josh Heinrichs (jih320) <josh.heinrichs@usask.ca>
2015-06-02 15:37:33 -0600

    Moved Triangle angle related methods into Angle class This is so that other classes can use them, namely to allow DelaunayTriangulation to calculate alpha-stability

--- src/main/java/geometry/Triangle.java
+++ src/main/java/geometry/Triangle.java
@@ -1,7 +1,5 @@
 package geometry;
 
-import constants.Constants;
-
 import java.util.ArrayList;
 
 /**
@@ -83,72 +81,30 @@ public class Triangle {
     }
 
     public double getAngleA() {
-        return getAngle(c, a, b);
+        return Angle.getAngle(c, a, b);
     }
 
     public double getAngleAStart() {
-        return getAngleStart(c, a, b);
+        return Angle.getAngleStart(c, a, b);
     }
 
     public double getAngleB() {
-        return getAngle(a, b, c);
+        return Angle.getAngle(a, b, c);
     }
 
     public double getAngleBStart() {
-        return getAngleStart(a, b, c);
+        return Angle.getAngleStart(a, b, c);
     }
 
     public double getAngleC() {
-        return getAngle(b, c, a);
+        return Angle.getAngle(b, c, a);
     }
 
     public double getAngleCStart() {
-        return getAngleStart(b, c, a);
+        return Angle.getAngleStart(b, c, a);
     }
 
     public boolean isObtuse() {
         return (getAngleA() > 90.d || getAngleB() > 90.d || getAngleC() > 90.d);
-    }
-
-    /**
-     * Retruns the angle from 0 to the start of angle B
-     * @param A
-     * @param B
-     * @param C
-     * @return
-     */
-    static double getAngleStart(Point A, Point B, Point C) {
-
-        double angle1 = Math.toDegrees(Math.atan2(A.y - B.y, A.x - B.x));
-        double angle2 = Math.toDegrees(Math.atan2(C.y - B.y, C.x - B.x));
-
-        if(angle1 < 0) {
-            angle1 += 360.d;
-        }
-        if(angle2 < 0) {
-            angle2 += 360.d;
-        }
-
-        if( Math.abs(((angle1 + getAngle(A, B, C)) % 360) - angle2) < Constants.EPSILON ) {
-            return angle1;
-        } else {
-            assert( Math.abs(((angle2 + getAngle(A, B, C)) % 360) - angle1) < Constants.EPSILON ) : angle1 + ", " + angle2;
-            return angle2;
-        }
-    }
-
-    /**
-     * Returns the inner angle B from the given triangle
-     * @param A
-     * @param B
-     * @param C
-     * @return
-     */
-    static double getAngle(Point A, Point B, Point C) {
-        //law of cosines
-        double b = A.distance(C);
-        double a = C.distance(B);
-        double c = B.distance(A);
-        return Math.toDegrees(Math.acos((a * a + c * c - b * b) / (2.d * a * c)));
     }
 }
